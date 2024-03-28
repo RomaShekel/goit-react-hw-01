08f9a6232aeec9485f49e3a5f47202e5d7573716
RomaShekel <141637507+RomaShekel@users.noreply.github.com>
2024-03-28 10:22:58 +0200

    up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,16 @@
-// src/components/App.jsx
-
+// src/components/App/App.jsx
+import userData from "../../userData.json";
+import Profile from "../Profile/Profile.jsx";
 
 export default function App() {
   return (
     <div>
+      <Profile         
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}/>
     </div>
   );
 }
